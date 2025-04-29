be724328295b01dca1cc8f899d088bbbbc34bdea
Ahmadsani31 <ahmaddarma0@gmail.com>
2025-04-29 15:38:41 +0700

    update(camera): screen detail

--- src/app/(protected)/(tabs)/perjalanan.tsx
+++ src/app/(protected)/(tabs)/perjalanan.tsx
@@ -1,11 +1,12 @@
-import { Button, Modal, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Button, Modal, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useRouter } from "expo-router";
 import { useContext, useRef, useState } from "react";
 import Input from "@/components/Input";
 import InputArea from "@/components/InputArea";
 import ButtonCostum from "@/components/ButtonCostum";
-import { ModalRN } from "@/components/ModalRN";
-import { CameraView } from "expo-camera";
+import { CameraMode, CameraView } from "expo-camera";
+import { AntDesign, FontAwesome6 } from '@expo/vector-icons';
+import { Image } from "expo-image";
 
 export default function PerjalananScreen() {
   const router = useRouter();
@@ -14,6 +15,14 @@ export default function PerjalananScreen() {
   const [modalVisible, setModalVisible] = useState(false);
 
   const cameraRef = useRef<CameraView | null>(null);
+  const [uri, setUri] = useState<JSON | null>(null);
+
+  const takePicture = async () => {
+    const photo = await cameraRef.current?.takePictureAsync({imageType:"png",base64:true});
+    console.log('photo', photo?.uri);
+    setUri(photo?.uri);
+    setModalVisible(false)
+  };
 
   return (
     <View className="flex-1 bg-slate-300">
@@ -24,7 +33,13 @@ export default function PerjalananScreen() {
           <Text className="font-medium">BA Nomor</Text>
         </View>
 
-
+        {uri && <Image
+        
+          source={{ uri }}
+          className="ob"
+          contentFit="contain"
+          style={{  aspectRatio: 1 }}
+        />}
 
         <InputArea className="bg-gray-200" label="Kegiatan" placeholder="Jenis Kegiatan" value="" onChangeText={setKegiatan} />
         <ButtonCostum classname="bg-blue-500" title="Open Camera" onPress={() => setModalVisible(true)} />
@@ -34,13 +49,44 @@ export default function PerjalananScreen() {
         animationType="fade"
       >
 
-        <View className="flex-1 bg-slate-600 justify-center items-center">
-        <CameraView style={{
-          height:'100%',
-          aspectRatio:4/3
-        }} ratio={"4:3"} facing="back" />
+        <View className="flex-1 bg-slate-600 justify-center items-center ">
+          <CameraView style={{
+            flex: 1,
+            width: "100%",
+          }} ratio={"4:3"} facing="back" ref={cameraRef} >
+            {/* <View className="absolute flex-row bottom-10 justify-center">
+              <ButtonCostum classname="bg-stone-500 " title="close" />
+              <ButtonCostum classname="bg-red-500 " title="close" onPress={() => setModalVisible(false)} />
+            </View> */}
+            <View style={styles.shutterContainer}>
+              <Pressable>
+                <AntDesign name="picture" size={32} color="white" />
 
-          <ButtonCostum title="close" onPress={()=>setModalVisible(false)}/>
+              </Pressable>
+              <Pressable onPress={takePicture}>
+                {({ pressed }) => (
+                  <View
+                    style={[
+                      styles.shutterBtn,
+                      {
+                        opacity: pressed ? 0.5 : 1,
+                      },
+                    ]}
+                  >
+                    <View
+                      style={[
+                        styles.shutterBtnInner,
+                        {
+                          backgroundColor: "white",
+                        },
+                      ]}
+                    />
+                  </View>
+                )}
+              </Pressable>
+            </View>
+          </CameraView>
+
           {/* <CameraView className="flex-1" facing={'front'}>
             <View className="w-full m-96 bg-black/50">
               <TouchableOpacity onPress={()=>setModalVisible(false)}>
@@ -57,3 +103,41 @@ export default function PerjalananScreen() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+    width: "100%",
+  },
+  shutterContainer: {
+    position: "absolute",
+    bottom: 44,
+    left: 0,
+    width: "100%",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+  },
+  shutterBtn: {
+    backgroundColor: "transparent",
+    borderWidth: 5,
+    borderColor: "white",
+    width: 85,
+    height: 85,
+    borderRadius: 45,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  shutterBtnInner: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+  },
+});

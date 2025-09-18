8f70aa1b4b7e18320adf65f53e055e58d7a9f451
Adsa <adsani3102@gmail.com>
2025-09-18 08:33:19 +0700

    update version

--- src/components/NotifikasiNewVersion.tsx
+++ src/components/NotifikasiNewVersion.tsx
@@ -32,37 +32,39 @@ export default function NotifikasiNewVersion() {
     // call the function
   }, []);
 
+  if (visible === false) {
+    return null;
+  }
+
   return (
-    visible && (
-      <Modal animationType="slide" transparent={true} visible={visible}>
-        <View className="flex-1 items-center justify-end bg-black/50">
-          <View className="w-full rounded-t-lg bg-white">
-            <View className="rounded-t-lg bg-[#4F959D] p-5">
-              <View className="flex-row gap-2">
-                <Foundation name="alert" size={24} color={'#ff9966'} />
-                <Text className="text-xl font-bold text-white">Wajib Update</Text>
-              </View>
+    <Modal animationType="slide" transparent={true} visible={visible}>
+      <View className="flex-1 items-center justify-end bg-black/50">
+        <View className="w-full rounded-t-lg bg-white">
+          <View className="rounded-t-lg bg-[#4F959D] p-5">
+            <View className="flex-row gap-2">
+              <Foundation name="alert" size={24} color={'#ff9966'} />
+              <Text className="text-xl font-bold text-white">Wajib Update</Text>
             </View>
-            <View className="mb-10 gap-4 p-5">
-              <Text>
-                Versi terbaru aplikasi sudah tersedia dan dibutuhkan agar aplikasi tetap berjalan
-                optimal. Silakan update untuk melanjutkan.
-              </Text>
-              <View className="my-5">
-                <TouchableHighlight
-                  className="items-center rounded-3xl bg-[#205781] py-3"
-                  onPress={() => Linking.openURL(URL_PLAY_STORE)}>
-                  <View className="flex-row items-center gap-2">
-                    <MaterialIcons name="system-security-update" size={20} color="white" />
-                    <Text className="text-lg font-bold text-white">New Update</Text>
-                  </View>
-                </TouchableHighlight>
-              </View>
+          </View>
+          <View className="mb-10 gap-4 p-5">
+            <Text>
+              Versi terbaru aplikasi sudah tersedia dan dibutuhkan agar aplikasi tetap berjalan
+              optimal. Silakan update untuk melanjutkan.
+            </Text>
+            <View className="my-5">
+              <TouchableHighlight
+                className="items-center rounded-3xl bg-[#205781] py-3"
+                onPress={() => Linking.openURL(URL_PLAY_STORE)}>
+                <View className="flex-row items-center gap-2">
+                  <MaterialIcons name="system-security-update" size={20} color="white" />
+                  <Text className="text-lg font-bold text-white">New Update</Text>
+                </View>
+              </TouchableHighlight>
             </View>
           </View>
         </View>
-      </Modal>
-    )
+      </View>
+    </Modal>
   );
 }
 
--- src/components/NotifikasiNewVersionMinor.tsx
+++ src/components/NotifikasiNewVersionMinor.tsx
@@ -26,37 +26,39 @@ export default function NotifikasiNewVersionMinor() {
     await Updates.reloadAsync(); // Restart dengan update
   };
 
+  if (visible === false) {
+    return null;
+  }
+
   return (
-    visible && (
-      <Modal animationType="slide" transparent={true} visible={visible}>
-        <View className="flex-1 items-center justify-end bg-black/50">
-          <View className="w-full rounded-t-lg bg-white">
-            <View className="rounded-t-lg bg-gray-200 p-5">
-              <View className="flex-row gap-2">
-                <AntDesign name="exclamationcircle" size={24} color="black" />
-                <Text className="text-xl font-bold">Minor Update</Text>
-              </View>
+    <Modal animationType="slide" transparent={true} visible={visible}>
+      <View className="flex-1 items-center justify-end bg-black/50">
+        <View className="w-full rounded-t-lg bg-white">
+          <View className="rounded-t-lg bg-gray-200 p-5">
+            <View className="flex-row gap-2">
+              <AntDesign name="exclamationcircle" size={24} color="black" />
+              <Text className="text-xl font-bold">Minor Update</Text>
             </View>
-            <View className="gap-4 p-5">
-              <Text>
-                Kami baru saja merilis pembaruan kecil untuk meningkatkan stabilitas dan kenyamanan
-                penggunaan. Silakan update!
-              </Text>
-              <View className="my-7">
-                <TouchableHighlight
-                  className="items-center rounded-xl bg-[#205781] py-3"
-                  onPress={() => handleUpdate()}>
-                  <View className="flex-row items-center gap-2">
-                    <MaterialIcons name="system-security-update" size={20} color="white" />
-                    <Text className="text-lg font-bold text-white">Update</Text>
-                  </View>
-                </TouchableHighlight>
-              </View>
+          </View>
+          <View className="gap-4 p-5">
+            <Text>
+              Kami baru saja merilis pembaruan kecil untuk meningkatkan stabilitas dan kenyamanan
+              penggunaan. Silakan update!
+            </Text>
+            <View className="my-7">
+              <TouchableHighlight
+                className="items-center rounded-xl bg-[#205781] py-3"
+                onPress={() => handleUpdate()}>
+                <View className="flex-row items-center gap-2">
+                  <MaterialIcons name="system-security-update" size={20} color="white" />
+                  <Text className="text-lg font-bold text-white">Update</Text>
+                </View>
+              </TouchableHighlight>
             </View>
           </View>
         </View>
-      </Modal>
-    )
+      </View>
+    </Modal>
   );
 }
 
